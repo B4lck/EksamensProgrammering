b7ec4e16b5dd9aec2271b1c9f4c20c3275731d68
B4lck <nikolaisaldernbalck@gmail.com>
2023-03-23 13:56:19 +0100

    Init

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Eksamens projekt</title>
+</head>
+<body>
+    <canvas id="canvas" width="800px" height="600px"></canvas>
+    <script src="./main.js" type="module">
+
+    </script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let Camera = {
+
+}
+
+ctx.fillStyle = "Black";
+ctx.fillRect(0,0,canvas.width,canvas.height);
